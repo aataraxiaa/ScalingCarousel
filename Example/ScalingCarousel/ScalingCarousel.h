9436995265ccdac7f77182a716c46812532d8b16
Pete Smith <peadar81@gmail.com>
2018-02-10 18:55:36 +0000

    Add new framework to main project

---
+++ Example/ScalingCarousel/ScalingCarousel.h
@@ -0,0 +1,19 @@
+//
+//  ScalingCarousel.h
+//  ScalingCarousel
+//
+//  Created by Pete Smith on 10/02/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScalingCarousel.
+FOUNDATION_EXPORT double ScalingCarouselVersionNumber;
+
+//! Project version string for ScalingCarousel.
+FOUNDATION_EXPORT const unsigned char ScalingCarouselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScalingCarousel/PublicHeader.h>
+
+
