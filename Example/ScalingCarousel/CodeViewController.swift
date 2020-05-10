52eda36ce964e8252dc919044bd9744a3f02f857
Pete Smith <peadar81@gmail.com>
2020-05-10 12:22:21 +0100

    Call didScroll in sample app as per readMe

--- Example/ScalingCarousel/CodeViewController.swift
+++ Example/ScalingCarousel/CodeViewController.swift
@@ -102,6 +102,6 @@ extension CodeViewController: UICollectionViewDataSource {
 extension CodeViewController: UICollectionViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        //scalingCarousel.didScroll()
+        scalingCarousel.didScroll()
     }
 }
